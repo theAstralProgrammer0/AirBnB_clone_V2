577918fb83144e14abe4f1765425c77cabf50b76
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-04 05:39:38 +0100

    task 1 correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""This is a fabric script to compress a file with nomenclature"""
+"""This module contains a fabric script to compress a file with nomenclature"""
 from fabric.api import local
 from time import strftime
 from datetime import date
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    """This function generates a .tgz for the contents of the web_static dir"""
 
-    filename = "versions/web_static_{}.tgz".format(strftime("%Y%m%d%H%M%S"))
+    filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf {}".format(filename))
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
-        return filename
+        return "versions/web_static_{}.tgz".format(filename)
 
     except Exception as e:
         return None
