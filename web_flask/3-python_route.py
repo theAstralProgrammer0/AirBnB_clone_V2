ea5a1fc3f8d7e23128f151cddcdebb3ce5ea77c1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-20 02:02:14 +0100

    task 3 debugged

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -33,6 +33,7 @@ def c(text):
     return shlx(text, '_', 'C ')
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """This is the python endpoint function"""
