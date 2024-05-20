bd20eb4f8a584e4e3108235cb2eb329c4bf4173b
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-20 03:39:08 +0100

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""This module sets up a Flask application"""
+from flask import Flask, render_template
+from shlex import shlex
+
+
+def shlx(txt, delim=' ', prefix='', suffix=''):
+    """This function splits strings and prefixes or suffixes strings"""
+    lexer = shlex(txt)
+    lexer.whitespace += delim
+    words = list(lexer)
+    return prefix + ' '.join(words) + suffix
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """This is the home endpoint function"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This is the hbnb endpoint function"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """This is the c endpoint function"""
+    return shlx(text, '_', 'C ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """This is the python endpoint function"""
+    return shlx(text, '_', 'Python ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """This is the number endpoint function"""
+    return str(n) + " is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    """This is the number template endpoint function"""
+    return render_template('5-number.html', num=str(n))
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html', num=str(n), par='even')
+    else:
+        return render_template('6-number_odd_or_even.html', num=str(n), par='odd')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
