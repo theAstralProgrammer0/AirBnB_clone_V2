a9b7d23195048b12603bdbd5ded5c0dc528156cf
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-20 01:21:43 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module sets up a Flask application"""
+from flask import Flask
+from shlex import shlex
+
+
+def shlx(txt, delim=' ', prefix='', suffix=''):
+    """This function splits strings and prefixes or suffixes strings"""
+    lexer = shlex(txt)
+    lexer.whitespace += delim
+    words = list(lexer)
+    return prefix + ' '.join(words) + suffix
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """This is the home endpoint function"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This is the hbnb endpoint function"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """This is the c endpoint function"""
+    return shlx(text, '_', 'C ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """This is the python endpoint function"""
+    return shlx(text, '_', 'Python ')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
